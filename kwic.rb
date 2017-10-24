a5f00cbcfb6692dc10178bb20c063a732070108b
Ramon Voges <ramon.voges@posteo.de>
2017-10-24 11:06:12 +0200

    Modularisiert

--- kwic.rb
+++ kwic.rb
@@ -1,66 +1,68 @@
 #!/usr/bin/env ruby
 
-# Returns the 7-gram of a given keyword in the given files.
-class Kwic
-  def initialize(keyword)
-    @index = []
-    @keyword ||= keyword
-    @lines = []
-    @wordlist = []
-  end
-
-  def read_file
-    puts "You are looking for '#{@keyword}' in #{ARGV.length} file(s)."
-    puts
-    ARGF.each do |line|
-      @lines << line.split
+module Kwic
+  # Returns the 7-gram of a given keyword in the given files.
+  class Kwic
+    def initialize(keyword)
+      @index = []
+      @keyword ||= keyword
+      @lines = []
+      @wordlist = []
     end
-  end
 
-  def read_string(text)
-    @lines = text.split
-  end
-
-  def process
-    split_lines_into_words
-    search_keyword
-    print_keyword_in_context
-  end
-
-  def print_keyword_in_context
-    @index.each do |i|
-      printf '# %*d: ', number_length, i + 1
-      if i <= 4
-        start = 4 - i
-        printf '%*s', width_text_snippet, @wordlist[start, start + i].join(' ')
-      else
-        printf '%*s', width_text_snippet, @wordlist[i - 4, 4].join(' ')
+    def read_file
+      puts "You are looking for '#{@keyword}' in #{ARGV.length} file(s)."
+      puts
+      ARGF.each do |line|
+        @lines << line.split
       end
-      # printf '  %s  ', @wordlist[i]
-      print "  #{@wordlist[i]}  "
-      printf "%-*s\n", width_text_snippet, @wordlist[i + 1, 4].join(' ')
     end
-  end
 
-  private
+    def read_string(text)
+      @lines = text.split
+    end
 
-  def split_lines_into_words
-    @wordlist = @lines.flatten
-  end
+    def process
+      split_lines_into_words
+      search_keyword
+      print_keyword_in_context
+    end
 
-  def search_keyword
-    @wordlist.each_index { |i| @index << i if @wordlist[i].match(@keyword) }
-  end
+    def print_keyword_in_context
+      @index.each do |i|
+        printf '# %*d: ', number_length, i + 1
+        if i <= 4
+          start = 4 - i
+          printf '%*s', width_text_snippet, @wordlist[start, start + i].join(' ')
+        else
+          printf '%*s', width_text_snippet, @wordlist[i - 4, 4].join(' ')
+        end
+        # printf '  %s  ', @wordlist[i]
+        print "  #{@wordlist[i]}  "
+        printf "%-*s\n", width_text_snippet, @wordlist[i + 1, 4].join(' ')
+      end
+    end
 
-  def number_length
-    @wordlist.length.to_s.length
-  end
+    private
 
-  def width_text_snippet
-    (80 - @keyword.length - number_length) / 2
+    def split_lines_into_words
+      @wordlist = @lines.flatten
+    end
+
+    def search_keyword
+      @wordlist.each_index { |i| @index << i if @wordlist[i].match(@keyword) }
+    end
+
+    def number_length
+      @wordlist.length.to_s.length
+    end
+
+    def width_text_snippet
+      (80 - @keyword.length - number_length) / 2
+    end
   end
 end
 
-ngram = Kwic.new(ARGV.shift)
+ngram = Kwic::Kwic.new(ARGV.shift)
 ngram.read_file
 ngram.process
